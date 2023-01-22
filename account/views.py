352fe86cce8f72f262f152d5e815cd20e04e621e
WyRiXonImprove <wyrixonimprove@gmail.com>
2023-01-22 18:42:26 +0300

    kmk

--- account/views.py
+++ account/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic.edit import CreateView
+
+
+# Создаем здесь представления.
+def home(request):
+    return render(request, "registration/home.html")
 
 
 class SignUp(CreateView):
     form_class = UserCreationForm
-    success_url = '/'
-    template_name = "registration/sign_up.html"
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
