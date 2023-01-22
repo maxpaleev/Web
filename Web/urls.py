eb52b7f1faca5715401fedfd252831058d7a92ab
MaxPal <37022654+maxpaleev@users.noreply.github.com>
2023-01-22 18:33:15 +0300

    V 0.2 regis

--- Web/urls.py
+++ Web/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("signup/", include("account.urls")),
+    path('home/', include('account.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
