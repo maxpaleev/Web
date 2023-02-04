b4c280d03e7e318389f361a6d833c087a3223a2c
WyRiXonImprove <wyrixonimprove@gmail.com>
2023-02-04 16:01:28 +0300

    dsvnuisvbuwbv

--- Web/settings.py
+++ Web/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/profile'
 LOGOUT_REDIRECT_URL = "http://127.0.0.1:8000/"
 
 AUTH_USER_MODEL = 'account.User'
