950b83e41d24fcd9f2d7f2cb4d49e7632efc1f88
MaxPal <37022654+maxpaleev@users.noreply.github.com>
2023-02-05 19:47:53 +0300

    V 0.6 profile

--- Web/urls.py
+++ Web/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('account/', include('django.contrib.auth.urls')),
     path('reg/', include('account.urls')),
-    path('profile', include('profil.urls'))
+    path('profile/', include('profil.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
