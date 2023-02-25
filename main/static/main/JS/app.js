b4ed35fe17a2f3b3e29d1bd34f99109334ad8373
WyRiXonImprove <wyrixonimprove@gmail.com>
2023-02-25 20:50:32 +0300

    5555

--- main/static/main/JS/app.js
+++ main/static/main/JS/app.js
@@ -1,9 +1,6 @@
-console.log("34242");
 
 const TabsBut = document.querySelectorAll(".tabs__but");
 const TabItem = document.querySelectorAll(".box--information")
-
-console.log("34242");
 
 TabsBut.forEach(function(item) {
 	item.addEventListener("click", function() {
@@ -11,19 +8,18 @@ TabsBut.forEach(function(item) {
 		let TabId = curr.getAttribute("data-tab");
 		let CurrTab = document.querySelector(TabId);
 
-		if (!curr.classList.contains("active")) {
-			TabsBut.forEach(function(item) {
-				item.classList.remove("active")
-			});
+		
+		TabsBut.forEach(function(item) {
+		item.classList.remove("active")
+		});
+
+		TabItem.forEach(function(item) {
+		item.classList.remove("active")
+		});
 	
-			TabItem.forEach(function(item) {
-				item.classList.remove("active")
-			});
-	
-			curr.classList.add("active");
-			CurrTab.classList.add("active");
-		};
+		curr.classList.add("active");
+		CurrTab.classList.add('active');
+
 	});
 });
 
-document.getSelection("tabs__but").click()
